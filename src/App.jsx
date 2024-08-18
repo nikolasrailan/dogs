5ef2b8846db52f402b94a18e83d63f81800fdc7e
Nikolas <nikolasrailanmp@gmail.com>
2024-08-18 14:04:37 -0300

    rotas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtecdedRoute from "./Components/Helper/ProtecdedRoute";
 
 const App = () => {
   return (
@@ -15,7 +17,15 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtecdedRoute>
+                  <User />
+                </ProtecdedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
