bebbcfe681809133697328a015589cbf7aa316bd
Nikolas <nikolasrailanmp@gmail.com>
2024-09-04 11:09:49 -0300

    começando feed

---
+++ src/Components/Feed/FeedPhotos.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import useFetch from "../../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
+import FeedPhotosItem from "./FeedPhotosItem";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { response, json } = await request(url, options);
+      console.log(json);
+    }
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <ul>
+        {data.map((photo) => {
+          <FeedPhotosItem photo={photo} key={photo.id} />;
+        })}
+      </ul>
+    );
+  else return null;
+};
+
+export default FeedPhotos;
